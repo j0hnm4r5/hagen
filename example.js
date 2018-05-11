76e5810f8649c2b4daec8ff40e64829691a9726e
John Mars <john@M4R5.io>
2018-05-11 14:57:55 -0400

    :sparkles: Make text color contrast label colors

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 const hagen = require("./hagen");
 
-hagen.log("COMPONENT A", "Hello, World!");
-hagen.log("COMPONENT Z", "Hello, World!");
+hagen.log("COMPONENT", "Hello, World!");
+hagen.log("THING", "Hello, World!");
 
 hagen.warn("ABC", "Something happened!");
 hagen.error("123", "This is bad.");
--- hagen.js
+++ hagen.js
@@ -10,76 +10,78 @@ const chalk = require("chalk");
 
 // TODO: This isn't a super duper safe way to check for Browser/Node/: https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js
 // Ideally, we shouldn’t even have to check; the logging should work regardless of platform
-const isNode =
-  typeof process === "object" && `${process}` === "[object process]";
+const isNode = typeof process === "object" && `${process}` === "[object process]";
+
+const textColors = Object.freeze({
+	LIGHT: "#FAFAFA",
+	DARK: "212121",
+});
 
 const reservedColors = Object.freeze({
-  WARN: "#424242",
-  ERROR: "#212121",
-  TEXT: "#FAFAFA" // Gray
+	WARN: { bg: "#424242", text: textColors.LIGHT },
+	ERROR: { bg: "#212121", text: textColors.LIGHT },
 });
 
 const colors = [
-  // Google Material colors (A400): https://gist.github.com/kawanet/a880c83f06d6baf742e45ac9ac52af96
-  "#f50057", // Pink
-  "#d500f9", // Purple
-  "#651fff", // Deep Purple
-  "#3d5afe", // Indigo
-  "#2979ff", // Blue
-  "#00b0ff", // Light Blue
-  "#00e5ff", // Cyan
-  "#1de9b6", // Teal
-  "#00e676", // Green
-  "#76ff03", // Light Green
-  "#c6ff00", // Lime
-  "#ffea00", // Yellow
-  "#ff9100", // Orange
-  "#ff3d00" // Deep Orange
+	// Google Material colors (A400): https://gist.github.com/kawanet/a880c83f06d6baf742e45ac9ac52af96
+	{ bg: "#f50057", text: textColors.LIGHT }, // Pink
+	{ bg: "#d500f9", text: textColors.LIGHT }, // Purple
+	{ bg: "#651fff", text: textColors.LIGHT }, // Deep Purple
+	{ bg: "#3d5afe", text: textColors.LIGHT }, // Indigo
+	{ bg: "#2979ff", text: textColors.LIGHT }, // Blue
+	{ bg: "#00b0ff", text: textColors.DARK }, // Light Blue
+	{ bg: "#00e5ff", text: textColors.DARK }, // Cyan
+	{ bg: "#1de9b6", text: textColors.DARK }, // Teal
+	{ bg: "#00e676", text: textColors.DARK }, // Green
+	{ bg: "#76ff03", text: textColors.DARK }, // Light Green
+	{ bg: "#c6ff00", text: textColors.DARK }, // Lime
+	{ bg: "#ffea00", text: textColors.DARK }, // Yellow
+	{ bg: "#ff9100", text: textColors.DARK }, // Orange
+	{ bg: "#ff3d00", text: textColors.LIGHT }, // Deep Orange
 ];
 
 // Writes to console colored text, from the passed-in logger
 function logg(logger, color, label, message) {
-  // TODO: Try to get rid of the if...else, and just have a uniform logging method
-  if (isNode) {
-    logger(
-      chalk
-        .bgHex(color)
-        .hex(reservedColors.TEXT)
-        .bold(` ${label} `),
-      `${message}`
-    );
-  } else {
-    logger(
-      `%c ${label} %c ${message}`,
-      `background-color: ${color}; color: ${reservedColors.TEXT}`,
-      `background-color: inherit; color: inherit`
-    );
-  }
+	// TODO: Try to get rid of the if...else, and just have a uniform logging method
+	if (isNode) {
+		logger(
+			chalk
+				.bgHex(color.bg)
+				.hex(color.text)
+				.bold(` ${label} `),
+			`${message}`
+		);
+	} else {
+		logger(
+			`%c ${label} %c ${message}`,
+			`background-color: ${color.bg}; color: ${color.text}`,
+			`background-color: inherit; color: inherit`
+		);
+	}
 }
 
 // Basic logger
 function log(label, message) {
-  // Picks a random color from the array, based on the label name
-  let val = 0;
-  label.split("").forEach(character => {
-    val += character.charCodeAt(0);
-  });
+	// Picks a random color from the array, based on the label name
+	let val = 0;
+	label.split("").forEach((character) => {
+		val += character.charCodeAt(0);
+	});
 
-  const numColors = colors.length;
-  const color = colors[val % numColors];
+	const numColors = colors.length;
+	const color = colors[val % numColors];
 
-  logg(console.log, color, label, message);
+	logg(console.log, color, label, message);
 }
 
 // Warning logger
 function warn(label, message) {
-  logg(console.warn, reservedColors.WARN, `⚠️  ${label}`, message);
+	logg(console.warn, reservedColors.WARN, `⚠️  ${label}`, message);
 }
 
 // Error logger
 function error(label, message) {
-  logg(console.error, reservedColors.ERROR, `❌  ${label}`, message);
+	logg(console.error, reservedColors.ERROR, `❌  ${label}`, message);
 }
-
 
 module.exports = { log, warn, error };
