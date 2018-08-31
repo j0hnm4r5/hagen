14d2b584dbc019d65cb7f68e97f66a94c9df601c
John Mars <john@M4R5.io>
2018-08-31 10:03:19 -0400

    :sparkles: Add support for custom colors

--- hagen.js
+++ hagen.js
@@ -6,90 +6,100 @@
  *
  */
 
-import chalk from "chalk";
-// const chalk = require(`chalk`);
-import isNode from "detect-node";
-// const isNode = require(`detect-node`);
+// ========= IMPORTS =========
 
-import materialColors from "./utils/gColors";
-// const materialColors = require(`./utils/gColors`);
+const isNode = require(`detect-node`);
+
+// ========= COLORS =========
+const materialColors = require(`./utils/gColors`);
 
 const textColors = Object.freeze({
-    LIGHT: materialColors.gray[50],
-    DARK: materialColors.gray[900],
+	LIGHT: materialColors.gray[50],
+	DARK: materialColors.gray[900],
 });
 
 const reservedColors = Object.freeze({
-    WARN: { bg: materialColors.gray[500], text: textColors.DARK },
-    ERROR: { bg: materialColors.gray[900], text: textColors.LIGHT },
+	WARN: { bg: materialColors.gray[500], text: textColors.DARK },
+	ERROR: { bg: materialColors.gray[900], text: textColors.LIGHT },
 });
 
 const colors = [
-    { bg: materialColors.blue[500], text: textColors.LIGHT }, // blue
-    { bg: materialColors.red[500], text: textColors.LIGHT }, // red
-    { bg: materialColors.yellow[500], text: textColors.DARK }, // yellow
-    { bg: materialColors.green[500], text: textColors.LIGHT }, // green
-    { bg: materialColors.orange[500], text: textColors.LIGHT }, // orange
-    { bg: materialColors.pink[500], text: textColors.LIGHT }, // pink
-    { bg: materialColors.purple[500], text: textColors.LIGHT }, // purple
-    { bg: materialColors.cyan[500], text: textColors.DARK }, // cyan
+	{ bg: materialColors.blue[500], text: textColors.LIGHT }, // blue
+	{ bg: materialColors.red[500], text: textColors.LIGHT }, // red
+	{ bg: materialColors.yellow[500], text: textColors.DARK }, // yellow
+	{ bg: materialColors.green[500], text: textColors.LIGHT }, // green
+	{ bg: materialColors.orange[500], text: textColors.LIGHT }, // orange
+	{ bg: materialColors.pink[500], text: textColors.LIGHT }, // pink
+	{ bg: materialColors.purple[500], text: textColors.LIGHT }, // purple
+	{ bg: materialColors.cyan[500], text: textColors.DARK }, // cyan
 ];
+
+// ========= HELPERS =========
 
 // Writes to console colored text, from the passed-in logger
 function logg(logger, color, label, message) {
-    if (isNode) {
-        logger(
-            chalk
-                .bgHex(color.bg)
-                .hex(color.text)
-                .bold(` ${label} `),
-            `${message}`
-        );
-    } else {
-        logger(
-            `%c ${label} %c ${message}`,
-            `background-color: ${color.bg}; color: ${color.text}`,
-            `background-color: inherit; color: inherit`
-        );
-    }
+	if (isNode) {
+		const chalk = require(`chalk`);
+		logger(
+			chalk
+				.bgHex(color.bg)
+				.hex(color.text)
+				.bold(` ${label} `),
+			`${message}`
+		);
+	} else {
+		logger(
+			`%c ${label} %c ${message}`,
+			`background-color: ${color.bg}; color: ${color.text}`,
+			`background-color: inherit; color: inherit`
+		);
+	}
 }
 
-// Basic logger
+// ========= LOGGERS =========
+
+// basic logger
 function log(label, message, manualColor) {
-    // Picks a random color from the array, based on the label name
-    let val = 0;
-    label.split(``).forEach((character) => {
-        val += character.charCodeAt(0);
-    });
+	let color;
 
-    const numColors = colors.length;
-    let color;
-    if (
-        manualColor !== undefined &&
-        typeof manualColor === `number` &&
-        manualColor >= 0
-    ) {
-        color = colors[Math.round(manualColor) % numColors];
-    } else {
-        color = colors[val % numColors];
-    }
+	// checks if a color id was passed in and sets the color
+	const numColors = colors.length;
+	if (typeof manualColor === `number` && manualColor >= 0) {
+		// if it was with an integer, use that as the color id
+		color = colors[Math.round(manualColor) % numColors];
+	} else if (
+		typeof manualColor === `object` &&
+		manualColor.bg !== undefined &&
+		manualColor.text !== undefined
+	) {
+		// if it was with a custom color object, use that
+		color = { bg: manualColor.bg, text: manualColor.text };
+	} else {
+		// if it wasn't, use a value generated from the label
+		let val = 0;
+		label.split(``).forEach((character) => {
+			val += character.charCodeAt(0);
+		});
 
-    logg(console.log, color, label, message);
+		color = colors[val % numColors];
+	}
+
+	// extends console.log()
+	logg(console.log, color, label, message);
 }
 
-// Warning logger
+// warning logger
 function warn(label, message) {
-    logg(console.warn, reservedColors.WARN, `⚠️  ${label}`, message);
+	// extends console.warn()
+	logg(console.warn, reservedColors.WARN, `⚠️  ${label}`, message);
 }
 
-// Error logger
+// error logger
 function error(label, message) {
-    logg(
-        console.error,
-        reservedColors.ERROR,
-        `❌  ${label}`,
-        message
-    );
+	// extends console.error()
+	logg(console.error, reservedColors.ERROR, `❌  ${label}`, message);
 }
+
+// ========= EXPORTS =========
 
 module.exports = { log, warn, error };
--- test/node.js
+++ test/node.js
@@ -1,4 +1,4 @@
-const hagen = require(`../index`);
+const hagen = require(`..`);
 
 // test label colors
 hagen.log(`COMPONENT`, `Hello, World!`);
@@ -24,8 +24,27 @@ hagen.log(`COLOR`, `This is color 10`, 10);
 hagen.log(`COLOR`, `This is color 11`, 11);
 hagen.log(`COLOR`, `This is color 12`, 12);
 
+// test custom colors
+hagen.log(`COLOR`, `This is color {bg: "#ff00ff", text: "#00ff00"}`, {
+	bg: `#ff00ff`,
+	text: `#00ff00`,
+});
+hagen.log(`COLOR`, `This is color {bg: "white", text: "black"}`, {
+	bg: `white`,
+	text: `black`,
+});
+
 // test weirdness
 hagen.log(`COLOR`, `This is color -1`, -1);
 hagen.log(`COLOR`, `This is color 0.5`, 0.5);
 hagen.log(`COLOR`, `This is color ABC`, `ABC`);
+hagen.log(`COLOR`, `This is color {}`, {});
+hagen.log(`COLOR`, `This is color {test: 123}`, { test: 123 });
+hagen.log(`COLOR`, `This is color {bg: "red", test: 123}`, {
+	bg: `red`,
+	test: 123,
+});
+hagen.warn(`COLOR`, `This is color 0`, 0);
+hagen.error(`COLOR`, `This is color 0`, 0);
+hagen.warn(`COLOR`, `This is color ABC`, `ABC`);
 hagen.error(`COLOR`, `This is color ABC`, `ABC`);
