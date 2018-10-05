974ae7619cbde86729273bf26df9b792959134f2
John Mars <jmars@deeplocal.com>
2018-10-05 16:35:17 -0400

    Add info, better icons, better coloring, and bug fixes

--- hagen.js
+++ hagen.js
@@ -8,11 +8,11 @@
 
 // ========= IMPORTS =========
 
-const isNode = require("detect-node");
-const chalk = require("chalk");
+const isNode = require(`detect-node`);
+const chalk = require(`chalk`);
 
 // ========= COLORS =========
-const colorList = require("./utils/colors");
+const colorList = require(`./utils/colors`);
 
 const textColors = Object.freeze({
 	LIGHT: colorList.gray[50],
@@ -20,8 +20,9 @@ const textColors = Object.freeze({
 });
 
 const reservedColors = Object.freeze({
-	WARN: { bg: colorList.gray[500], text: textColors.DARK },
-	ERROR: { bg: colorList.gray[900], text: textColors.LIGHT },
+	WARN: { bg: `#ccff00`, text: textColors.DARK },
+	ERROR: { bg: `#ff0099`, text: textColors.LIGHT },
+	INFO: { bg: `#000000`, text: textColors.LIGHT },
 });
 
 const colors = [
@@ -40,6 +41,7 @@ const colors = [
 // Writes to console colored text, from the passed-in logger
 function logg(logger, color, label, message) {
 	if (isNode) {
+		// if using node.js
 		logger(
 			chalk
 				.bgHex(color.bg)
@@ -48,11 +50,52 @@ function logg(logger, color, label, message) {
 			`${message}`
 		);
 	} else {
-		logger(
-			`%c ${label} %c ${message}`,
-			`background-color: ${color.bg}; color: ${color.text}`,
-			`background-color: inherit; color: inherit`
-		);
+		// if using the browser
+		switch (typeof message) {
+			case `object`:
+				// if the message is an object
+				logger(
+					`%c ${label} %c %o`,
+					`
+						background-color: ${color.bg}; 
+						color: ${color.text}; 
+						font-weight: bold;
+					`,
+					`
+						background-color: inherit; 
+						color: inherit; 
+						font-weight: inherit;
+					`,
+					message
+				);
+				break;
+			case `undefined`:
+				// if there is no message
+				logger(
+					`%c ${label} `,
+					`
+						background-color: ${color.bg}; 
+						color: ${color.text}; 
+						font-weight: bold;
+					`
+				);
+				break;
+			default:
+				logger(
+					`%c ${label} %c ${message}`,
+					`
+						background-color: ${color.bg}; 
+						color: ${color.text}; 
+						font-weight: bold;
+					`,
+					`
+						background-color: inherit; 
+						color: inherit; 
+						font-weight: inherit;
+					`
+				);
+				break;
+		}
 	}
 }
 
@@ -60,12 +103,15 @@ function logg(logger, color, label, message) {
 
 // basic logger
 function log(label, message, manualColor) {
-	let color;
+	// if the label isn't a string
+	if (typeof label !== `string`) label = ``;
 
 	// checks if a color id was passed in and sets the color
+	let color;
 	const numColors = colors.length;
 	if (typeof manualColor === `number` && manualColor >= 0) {
 		// if it was with an integer, use that as the color id
+		// round floats to the nearest integer
 		color = colors[Math.round(manualColor) % numColors];
 	} else if (
 		typeof manualColor === `object` &&
@@ -84,22 +130,44 @@ function log(label, message, manualColor) {
 		color = colors[val % numColors];
 	}
 
-	// extends console.log()
 	logg(console.log, color, label, message);
+}
+
+// info logger
+function info(label, message) {
+	logg(console.log, reservedColors.INFO, `i ${label}`, message);
 }
 
 // warning logger
 function warn(label, message) {
-	// extends console.warn()
-	logg(console.warn, reservedColors.WARN, `⚠️  ${label}`, message);
+	logg(console.warn, reservedColors.WARN, `! ${label}`, message);
 }
 
 // error logger
 function error(label, message) {
-	// extends console.error()
-	logg(console.error, reservedColors.ERROR, `❌  ${label}`, message);
+	logg(console.error, reservedColors.ERROR, `✕ ${label}`, message);
+}
+
+// ========= GROUPS =========
+
+function group() {
+	console.group();
+}
+function groupCollapsed() {
+	console.groupCollapsed();
+}
+function groupEnd() {
+	console.groupEnd();
 }
 
 // ========= EXPORTS =========
 
-module.exports = { log, warn, error };
+module.exports = {
+	log,
+	info,
+	warn,
+	error,
+	group,
+	groupCollapsed,
+	groupEnd,
+};
